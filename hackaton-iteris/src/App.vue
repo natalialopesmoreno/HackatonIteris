417c4656a79038e28f2a2d441cfb71c4c94125b5
Natália Moreno <natalialopesmoreno@gmail.com>
2020-12-12 14:27:34 -0300

    Lista Crianças

--- hackaton-iteris/src/App.vue
+++ hackaton-iteris/src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  
+    <CriancaLista/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CriancaLista from "./views/CriancaLista.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CriancaLista
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ hackaton-iteris/src/views/CriancaLista.vue
@@ -0,0 +1,84 @@
+
+<template >
+    <div class="divTudo">
+    <v-simple-table class= "tabela" style="margin-top:50px; width:1200px; margin-left: 100px; margin-right:100px;">
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+           Nome
+          </th>
+          <th class="text-left">
+            Endereço
+          </th>
+          <th class="text-left">
+            Cartas
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="crianca of criancasLista" :key="crianca.nome"
+        >
+        <td>
+            <v-avatar size="100">
+              <img :src="crianca.photo" :alt="crianca.nome" style=" border: solid;
+  border-radius: 4px;
+  padding: 5px;
+  width: 150px;"/>
+            </v-avatar>
+            <span class="pl-2">{{ crianca.nome }}</span>
+          </td>
+          <td class="text-right">{{ crianca.address}}</td>
+          <td class="text-right">{{ crianca.description}}</td>
+        </tr>
+        <tr>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'CriancaLista',
+    data() {
+    return {
+      criancasLista: []
+    }
+    },
+
+    created() {
+    fetch('https://it3yui.firebaseio.com/natal.json')
+      .then(response => response.json())
+      .then(json => {
+        this.criancasLista = json;
+      })
+  }
+    
+}
+</script>
+
+<style scoped>
+td
+{
+  width:500px;
+  background-color: rgb(116, 224, 116);
+  
+}
+
+
+
+th
+{
+  background-color:red;
+}
+
+.divTudo
+{
+  background-color:rgb(238, 120, 120);
+}
+
+</style>
